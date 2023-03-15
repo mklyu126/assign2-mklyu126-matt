c4cdfc22d7c34efbeee836b7d7a228e25f0cb35b
Matthew Do <mdo709@mtroyal.ca>
2023-03-15 13:32:54 -0600

    upadted javadoc

--- Assignment 2/src/exceptions/NegativeInputPrice.java
+++ Assignment 2/src/exceptions/NegativeInputPrice.java
@@ -1,9 +1,15 @@
 package exceptions;
 
 
-
+/**
+ * this is an execption class that tells the user they entered a negative number
+ * @author Max
+ *
+ */
 public class NegativeInputPrice extends Exception {
-
+/**
+ * this class informs the user they entered a negative number
+ */
 	public NegativeInputPrice() {
 		super("Error: Negative input price");
 	}
